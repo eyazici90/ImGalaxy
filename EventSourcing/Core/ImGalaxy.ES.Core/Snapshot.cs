2e662d69f5c36b59a4c99a7d0e6bb0d03d26b796
Emre YAZICI <iemreyazici@suitsupply.com>
2019-07-01 11:35:55 +0200

    snapshot.cs adds

---
+++ EventSourcing/Core/ImGalaxy.ES.Core/Snapshot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImGalaxy.ES.Core
+{
+    public class Snapshot
+    {
+        public int Version { get; private set; }
+
+        public object State { get; private set; }
+
+        public Snapshot(int version, object state)
+        {
+            Version = version;
+            State = state;
+        }
+    }
+}
