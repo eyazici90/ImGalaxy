737f6fa6ae3b61b876b9f6d10d011af040c55217
Emre YAZICI <iemreyazici@suitsupply.com>
2019-07-03 17:34:52 +0200

    projectionHandler adds

--- EventSourcing/EventStore/ImGalaxy.ES.EventStore/ProjectionHandler.cs
+++ EventSourcing/EventStore/ImGalaxy.ES.EventStore/ProjectionHandler.cs
@@ -5,16 +5,16 @@ using System.Threading.Tasks;
 
 namespace ImGalaxy.ES.EventStore
 {
-    public abstract class SubscriptionHandler
+    public abstract class ProjectionHandler
     {
         readonly Type _type;
 
-        protected SubscriptionHandler() => _type = GetType();
+        protected ProjectionHandler() => _type = GetType();
 
         public abstract Task Handle(object e);
 
         public override string ToString() => _type.Name;
 
-        public static implicit operator string(SubscriptionHandler self) => self.ToString();
+        public static implicit operator string(ProjectionHandler self) => self.ToString();
     }
 }
