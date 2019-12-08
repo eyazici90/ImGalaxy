4c4cc5e12366cc78a5a5ab886fec907a984e74af
eyazici <e.yazici1990@gmail.com>
2019-12-08 15:34:05 +0100

    version object added

---
+++ EventSourcing/src/ImGalaxy.ES.Core/Version.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImGalaxy.ES.Core
+{
+    public class Version
+    {
+        public long Value { get; }
+        public string MetaData { get; }
+
+        private Version(long value,
+            string metaData)
+        {
+            Value = value;
+            MetaData = metaData;
+        }
+
+        public Version(long value) : this(value, default)
+        {
+        }
+
+        public void WithMetaData(string metaData) =>
+            new Version(this.Value, metaData);
+
+        public static implicit operator long(Version self) => self.Value;
+    }
+}
