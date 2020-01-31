85700a9ec220f5ca03c6126515819ae6ae9b13f4
Emre YAZICI <IEmreYazici@suitsupply.com>
2020-01-31 11:14:42 +0100

    commandHandlerBase update

--- EventSourcing/src/ImGalaxy.ES.Core/CommandHandlerBase.cs
+++ EventSourcing/src/ImGalaxy.ES.Core/CommandHandlerBase.cs
@@ -6,17 +6,32 @@ namespace ImGalaxy.ES.Core
     public abstract class CommandHandlerBase<TAggregateRoot, TKey>
         where TAggregateRoot : class, IAggregateRootState<TAggregateRoot>, IAggregateRoot
     {
-        public IUnitOfWork UnitOfWork { get; }
         public IAggregateRootRepository<TAggregateRoot> RootRepository { get; }
-        public CommandHandlerBase(IUnitOfWork unitOfWork, IAggregateRootRepository<TAggregateRoot> rootRepository)
+
+        private readonly Func<Task<IExecutionResult>> _saveChanges;
+
+        private CommandHandlerBase(Func<Task<IExecutionResult>> saveChanges,
+            IAggregateRootRepository<TAggregateRoot> rootRepository)
         {
-            UnitOfWork = unitOfWork;
+            _saveChanges = saveChanges;
             RootRepository = rootRepository;
         }
+
+        public CommandHandlerBase(IUnitOfWork unitOfWork,
+            IAggregateRootRepository<TAggregateRoot> rootRepository)
+            : this(async () => await unitOfWork.SaveChangesAsync(), rootRepository)
+        {
+        }
+
+        public CommandHandlerBase(IAggregateRootRepository<TAggregateRoot> rootRepository)
+             : this(async () => ExecutionResult.Success, rootRepository)
+        {
+        }
+
         public virtual async Task<IExecutionResult> AddAsync(Func<Task<TAggregateRoot>> factory, string id) =>
             await AwaitTaskWithPrePostAction(factory, async root => await RootRepository.AddAsync(root, id),
-                        async root => await UnitOfWork.SaveChangesAsync()); 
-     
+                        async root => await _saveChanges());
+
         public virtual async Task<IExecutionResult> UpdateAsync(TKey id, Func<TAggregateRoot, Task> when) =>
             await AwaitTaskWithPrePostAction(async () =>
             {
@@ -24,11 +39,11 @@ namespace ImGalaxy.ES.Core
                 existingRoot.ThrowsIf(root => !root.HasValue, new AggregateNotFoundException(id.ToString()));
                 return existingRoot.Value;
             },
-            async a => await when(a),
-            async root => await UnitOfWork.SaveChangesAsync()); 
+            when,
+            async root => await _saveChanges());
 
         private async Task<IExecutionResult> AwaitTaskWithPrePostAction(Func<Task<TAggregateRoot>> preAction,
-            Func<TAggregateRoot, Task> realAction, 
+            Func<TAggregateRoot, Task> realAction,
             Func<TAggregateRoot, Task> postAction)
         {
             var aggregate = await preAction();
@@ -39,7 +54,7 @@ namespace ImGalaxy.ES.Core
 
             return ExecutionResult.Success;
         }
-        private async Task<Optional<TAggregateRoot>> FindByIdAsync(TKey id) => await RootRepository.GetAsync(id.ToString()); 
+        private async Task<Optional<TAggregateRoot>> FindByIdAsync(TKey id) => await RootRepository.GetAsync(id.ToString());
 
     }
 }
