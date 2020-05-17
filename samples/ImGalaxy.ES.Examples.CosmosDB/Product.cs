8285c6b9859938fb061136e151dbabbd1cbefdbb
Emre YAZICI <IEmreYazici@suitsupply.com>
2020-05-17 21:37:09 +0200

    Basic cosmos example added

---
+++ samples/ImGalaxy.ES.Examples.CosmosDB/Product.cs
@@ -0,0 +1,69 @@
+﻿using ImGalaxy.ES.Core;
+using System;
+using System.Collections.Generic;
+using static ImGalaxy.ES.Examples.CosmosDB.Product.Events;
+
+namespace ImGalaxy.ES.Examples.CosmosDB
+{
+    public static class Product
+    {
+        public class State : AggregateRootState<State>
+        {
+            public ProductId ProductId { get; private set; }
+            public ProductCode ProductCode { get; private set; }
+            internal State()
+            {
+                RegisterEvent<ProductCreated>(When);
+            }
+
+            private void When(ProductCreated @event) =>
+                With(this, state =>
+                {
+                    ProductId = new ProductId(@event.ProductId);
+                    ProductCode = new ProductCode(@event.Code);
+                });
+        }
+        public static State.Result Create(ProductId productId,
+            ProductCode productCode) =>
+            new State().ApplyEvent(new ProductCreated(productId, productCode.Value));
+
+
+        public class Events
+        {
+            public class ProductCreated
+            {
+                public string ProductId { get; }
+                public string Code { get; }
+                public ProductCreated(string productId,
+                    string code)
+                {
+                    ProductId = productId;
+                    Code = code;
+                }
+            }
+        }
+
+        public class ProductId : Identity<string>
+        {
+            public static ProductId New => new ProductId(Guid.NewGuid().ToString());
+            public ProductId(string id) : base(id)
+            {
+            }
+            public static implicit operator string(ProductId self) => self.Id;
+        }
+        public class ProductCode : ValueObject
+        {
+            public string Value { get; }
+            public ProductCode(string value)
+            {
+                Value = value;
+            }
+
+            protected override IEnumerable<object> GetAtomicValues()
+            {
+                yield return Value;
+            }
+
+        }
+    }
+}
---
+++ samples/ImGalaxy.ES.Examples.CosmosDB/Program.cs
@@ -0,0 +1,48 @@
+﻿using ImGalaxy.ES.Core;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace ImGalaxy.ES.Examples.CosmosDB
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var provider = BuildServiceProviderWithCosmosModule();
+
+            var aggregateStore = provider.GetRequiredService<IAggregateStore>();
+
+            var productId = Product.ProductId.New;
+
+
+            var result = Product.Create(productId, new Product.ProductCode("P012")); 
+
+            await aggregateStore.Save(result.State.ProductId, ExpectedVersion.NoStream, result).ConfigureAwait(false);
+
+            var aggregate = await aggregateStore.Load<Product.State>(productId).ConfigureAwait(false);
+
+            var state = aggregate.Root as Product.State;
+
+
+
+            Console.WriteLine($"Product Code : {state.ProductCode}");
+            Console.ReadLine();
+
+        }
+        static IServiceProvider BuildServiceProviderWithCosmosModule()
+        {
+            var services = new ServiceCollection();
+
+            services.AddImGalaxyESCosmosDBModule(config =>
+            {
+                config.DatabaseId = "TestCosmosES";
+                config.EndpointUri = "https://localhost:8081";
+                config.PrimaryKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+                config.ReadBatchSize = 1000;
+            });
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
