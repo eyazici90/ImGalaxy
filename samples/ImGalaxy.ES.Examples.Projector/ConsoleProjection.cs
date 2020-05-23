72019e7906913f6b56a0da3fc31e1cac764537a3
Emre YAZICI <IEmreYazici@suitsupply.com>
2020-05-23 20:23:40 +0200

    projector sample adds

---
+++ samples/ImGalaxy.ES.Examples.Projector/ConsoleProjection.cs
@@ -0,0 +1,31 @@
+﻿using ImGalaxy.ES.Projector;
+using System;
+
+namespace ImGalaxy.ES.Examples.Projector
+{ 
+    public class ConsoleProjection : Projection<ConsoleConnector>
+    {
+        public ConsoleProjection()
+        {
+            When<FakeEvent>(async (@event, connector) =>
+            {
+                connector.Write(@event.Name);
+            });
+        } 
+    }
+
+    public class FakeEvent
+    {
+        public string Name { get; }
+        public FakeEvent(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class ConsoleConnector
+    {
+        public void Write(string message) =>
+            Console.WriteLine(message);
+    }
+}
---
+++ samples/ImGalaxy.ES.Examples.Projector/Program.cs
@@ -0,0 +1,19 @@
+﻿using ImGalaxy.ES.Projector; 
+using System.Threading.Tasks;
+
+namespace ImGalaxy.ES.Examples.Projector
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connector = new ConsoleConnector();
+
+            var projector = new ConnectedProjector<ConsoleConnector>(connector, _ => new ConsoleProjection());
+
+            var @event = new FakeEvent("Amsterdam");
+
+            await projector.ProjectAsync(@event).ConfigureAwait(false); 
+        }
+    }
+}
